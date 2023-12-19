af0686f79840cd03d793fe2e7c4bc9a84415674b
Leoni <leoni.murilo@hotmail.com>
2023-12-19 17:16:23 -0300

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
 import Champions from "./resources/Champions.jsx";
+import ChampionPage from "./pages/ChampionPage";
+
+// declaração das rotas/páginas:
+// https://reactrouter.com/en/main/start/tutorial
+const routes = [
+  // Home page:
+  {
+    path: "/",
+    element: (
+      <>
+        <div className="head">
+          <div className="title">
+            <h1>LoL Champs</h1>
+          </div>
+          <div>
+            <img src="favicon.png" className="icone" alt="Ícone"></img>
+          </div>
+        </div>
+        <div>
+          <Champions />
+        </div>
+      </>
+    ),
+  },
+  // página do champ:
+  {
+    path: "/champions/:championId",
+    element: <ChampionPage />,
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <div>
-      <div className='head'>
-        <div className="title" >
-          <h1>LoL Champs</h1>     
-        </div>
-        <div>
-          <img src='favicon.png' className='icone' alt='Ícone'></img>
-        </div>
-      </div> 
-      <div >
-        <Champions />
-      </div>
+      <RouterProvider router={router} />
     </div>
   );
 }
