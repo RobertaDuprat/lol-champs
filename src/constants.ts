a188caa7002f399c6c650054c515660aea3cee1a
Leoni <leoni.murilo@hotmail.com>
2024-01-04 15:11:53 -0300

    Página de erro e chamada de API do campeao

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const API_BASE = "https://ddragon.leagueoflegends.com/cdn/13.23.1";
 export const API_URL = `${API_BASE}/data/en_US/champion.json`;
+export const CHAMPION_API_URL = `${API_BASE}/data/en_US/champion/`;
+// https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/item.json
